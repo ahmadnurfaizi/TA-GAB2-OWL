323c507d16117a6428d8c313f8143f0a03d2f8a9
ahmadnurfaizi <ahmadnurfaizi357>
2023-01-20 01:40:21 +0700

    master: membuat routes api konsumen

--- kavlingApi/routes/api.php
+++ kavlingApi/routes/api.php
@@ -16,6 +16,13 @@ use App\Http\Controllers\UsersController;
 |
 */
 
+// konsumen
+Route::post('konsumen/store', [KonsumenController::class,'store']);
+Route::get('konsumen', [KonsumenController::class,'index']);
+Route::post('konsumen/update/{id}', [KonsumenController::class,'update']);
+Route::get('konsumen/destroy/{id}', [KonsumenController::class,'destroy']);
+Route::get('konsumen/show/{id}', [KonsumenController::class,'show']);
+
 // user
 Route::post('users/store', [UsersController::class,'store']);
 Route::get('users', [UsersController::class,'index']);
@@ -23,6 +30,7 @@ Route::post('users/update/{id}', [UsersController::class,'update']);
 Route::get('users/destroy/{id}', [UsersController::class,'destroy']);
 Route::get('users/show/{id}', [UsersController::class,'show']);
 
+// login
 Route::post('users/login', [UsersController::class,'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {

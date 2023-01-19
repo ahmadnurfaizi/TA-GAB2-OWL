13f5f5fa3fa60bf2fc2291aefc72c5e7139f703e
ahmadnurfaizi <ahmadnurfaizi357>
2023-01-19 17:46:46 +0700

    master: membuat controller konsumen

---
+++ kavlingApi/app/Http/Controllers/KonsumenController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Konsumen;
+use Illuminate\Http\Request;
+use App\Helpers\ApiFormatter;
+use Exception;
+use Illuminate\Support\Facades\Hash;
+
+class KonsumenController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+
+            $data = Konsumen::where('username', '=', $request->username)->where('password', '=', $request->password)->get();
+
+            if ($data) {
+                return ApiFormatter::createApi(200, 'Success Login', $data);
+            } else {
+                return ApiFormatter::createApi(400, 'konsumen/Password Wrong!');
+            }
+        } catch (Exception) {
+            return ApiFormatter::createApi(400, 'Failed');
+        }
+    }
+
+}

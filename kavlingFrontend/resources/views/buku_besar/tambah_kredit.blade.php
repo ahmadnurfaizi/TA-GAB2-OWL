02e7ce00cd48e0cf2aa368cb38cf0cd75fca5127
tianasilviani16 <tianasilviani>
2023-01-20 13:48:41 +0700

    tiana: membuat tambah kredit pada buku_besar

---
+++ kavlingFrontend/resources/views/buku_besar/tambah_kredit.blade.php
@@ -0,0 +1,50 @@
+@extends('home')
+@section('content')
+
+<section class="content mt-3">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-6">
+                    <!-- general form elements -->
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Tambah Kredit Buku Besar</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form method="post" action="tambah" autocomplete="off">
+                            @csrf
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Tanggal Treansaksi</label>
+                                    <input type="date" name="tanggal" class="form-control" id="exampleInputEmail1"
+                                       >
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Item Transaksi</label>
+                                    <input type="text" class="form-control" name="item" id="exampleInputItem Transaksi1"
+                                        placeholder="Item Transaksi">
+                                </div>                                
+                                  <div class="form-group">
+                                    <label for="exampleInputPassword1">Nominal</label>
+                                    <input type="text" class="form-control" name="kredit" id="exampleInputNominal1"
+                                        placeholder="Nominal">
+                                </div>
+                                <div class="form-group">
+                                    <label>Keterangan</label>
+                                    <textarea class="form-control" name="keterangan" rows="3" placeholder="Keterangan ..."></textarea>
+                                  </div>
+                            </div>
+                            <!-- /.card-body -->
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Tambah</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection

634f86859d6f061412bcd2c7b2d0f463074f417e
ahmadnurfaizi <ahmadnurfaizi357>
2023-01-20 10:47:25 +0700

    master: fungsi pemanggilan

--- kavlingFrontend/routes/web.php
+++ kavlingFrontend/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AngsuranController;
+use App\Http\Controllers\KonsumenController;
+use App\Http\Controllers\Buku_besarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +20,35 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('login');
 // });
+
+// tampilan
+Route::get('home', function(){
+    return view('home');
+});
+Route::get('depan', function(){
+    return view('depan');
+});
+
+// buku_besar
+Route::get('buku_besar', [Buku_besarController::class,'index']);
+Route::post('buku_besar/tambah', [Buku_besarController::class,'store']);
+Route::get('buku_besar/tambah_kredit', [Buku_besarController::class,'create']);
+Route::get('buku_besar/tambah_debit', [Buku_besarController::class,'dcreate']);
+Route::get('buku_besar/hapus/{id}', [Buku_besarController::class,'destroy']);
+Route::get('buku_besar/edit/{id}', [Buku_besarController::class,'show']);
+Route::post('buku_besar/update/{id}', [Buku_besarController::class,'update']);
+
+// angsuran
+Route::get('angsuran', [AngsuranController::class,'index']);
+Route::get('angsuran/pdf', [AngsuranController::class, 'generatePDF']);
+Route::get('angsuran/tambah', [AngsuranController::class, 'create']);
+Route::post('angsuran/store', [AngsuranController::class, 'store']);
+
+// konsumen
+Route::get('konsumen', [KonsumenController::class,'index']);
+Route::get('konsumen/tambah', [KonsumenController::class,'create']);
+Route::post('konsumen/create/', [KonsumenController::class,'store']);
+Route::get('konsumen/hapus/{id}', [konsumenController::class,'destroy']);
 
 // login
 Route::get('/', [AuthController::class, 'login']);
